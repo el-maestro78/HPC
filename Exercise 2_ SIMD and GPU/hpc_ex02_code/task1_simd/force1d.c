c0e172b505cf3531a674ed803eed3e957415f1c6
CPP-Bot-78 <kzachoulitis@gmail.com>
2024-01-10 13:35:21 +0200

    task1 ready

--- Exercise 2_ SIMD and GPU/hpc_ex02_code/task1_simd/force1d.c
+++ Exercise 2_ SIMD and GPU/hpc_ex02_code/task1_simd/force1d.c
@@ -1,7 +1,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <sys/time.h>
-
+#include <assert.h>
 #include <immintrin.h>
 #include <x86intrin.h>
 #include <omp.h>
@@ -11,8 +11,8 @@
 2. gcc -o t1_w_opt force1d-ini.c -Ofast -fopt-info . Same as above, with O3 optimization.
 3. gcc -o t1_auto_vect force1d-ini.c -Ofast -ftree-vectorizer-verbose=6 -fopt-info . Same as above, auto-vectorized
 4. gcc -o t1_man_vect force1d.c -mavx -Ofast -Wall -fopt-info . This version of code, manual vector
-5. gcc -o t1_na force1d-ini_na.c -ftree-vectorizer-verbose=6 -Ofast -Wall -fopt-info. Same as 3 but with no dependancies
-testing gcc -o test test.c -mavx -O3 -Wall -fopt-info
+
+testing gcc -o test test.c -mavx -Wall -fopt-info
 */
 
 double get_wtime(void) {
@@ -25,56 +25,47 @@ double get_wtime(void) {
 const size_t N  = 1<<16; // system size
 const float eps = 5.0f;    // Lenard-Jones, eps
 const float rm  = 0.1f;   // Lenard-Jones, r_m
-
+float force_result[8] __attribute__((aligned(32))); //force_avx reduction
 
 /// compute the Lennard-Jones force particle at position x0
 float compute_force(float *positions, float x0)
 {
-	float rm2 = rm * rm;
-	float force = 0.0f;
-        /* old loop
-    for (size_t i=0; i<N; ++i) {
-      float r = x0 - positions[i];
-      float r2 = r * r; // r^2
-      float s2 = rm2 / r2; // (rm/r)^2
-      float s6 = s2*s2*s2; // (rm/r)^6
-      force += 12 * eps * (s6*s6 - s6) / r;
-    } //*/
-    __m256 x0_avx = _mm256_set1_ps(x0);
-    __m256 rm2_avx = _mm256_set1_ps(rm2);
-    __m256 eps_avx = _mm256_set1_ps(eps);
-    __m256 twelve_avx = _mm256_set1_ps(12.0f);
-    __m256 force_avx = _mm256_set1_ps(0.0f);
-    __m256 temp = _mm256_set1_ps(0.0f);
-    __m256 zeros = _mm256_set1_ps(0.0f);
+  float rm2 = rm * rm;
+  float force = 0.0f;
+  __m256 x0_avx = _mm256_set1_ps(x0);
+  const __m256 rm2_avx = _mm256_set1_ps(rm2);
+  const __m256 eps_avx = _mm256_set1_ps(eps);
+  const __m256 twelve_avx = _mm256_set1_ps(12.0f);
+  __m256 force_avx = _mm256_set1_ps(0.0f);
+  __m256 temp; //using temp to avoid dependencies
+  const __m256 zeros = _mm256_set1_ps(0.0f);
 
-    float force_result[8] __attribute__((aligned(32)));
-    //float force_result[8];
-    for (size_t i = 0; i+7 < N; i +=8) { // 4x8 =32 allignment for avx
-        __m256 pos_avx = _mm256_load_ps(positions+i);
-        __m256 r_avx = _mm256_sub_ps(x0_avx, pos_avx);
-        __m256 r2_avx = _mm256_mul_ps(r_avx, r_avx);
-            // (rm/r)^2 and (rm/r)^6
-        __m256 s2_avx = _mm256_div_ps(rm2_avx, r2_avx);
-        __m256 s6_avx = _mm256_mul_ps(_mm256_mul_ps(s2_avx, s2_avx), s2_avx);
-        //force Equation
-        __m256 twelve_eps_avx = _mm256_mul_ps(twelve_avx, eps_avx);
-        __m256 s6_sq_avx = _mm256_mul_ps(s6_avx,s6_avx);
-        __m256 s6_sq_sub_s6_avx =_mm256_sub_ps(s6_sq_avx, s6_avx);
-        __m256 force_fractor_avx = _mm256_div_ps(s6_sq_sub_s6_avx, r_avx);
-        __m256 force_contribution_avx = _mm256_mul_ps(twelve_eps_avx,force_fractor_avx);
-        //force_avx = _mm256_add_ps(force_avx, force_contribution_avx);
+  for (size_t i = 0; i+7 < N; i +=8) {
+    __m256 pos_avx = _mm256_load_ps(positions+i);
+    __m256 r_avx = _mm256_sub_ps(x0_avx, pos_avx);
+    __m256 r2_avx = _mm256_mul_ps(r_avx, r_avx);
+    // (rm/r)^2 and (rm/r)^6
+    __m256 s2_avx = _mm256_div_ps(rm2_avx, r2_avx);
+    __m256 s2_sq_avx = _mm256_mul_ps(s2_avx, s2_avx);
+    __m256 s6_avx = _mm256_mul_ps(s2_sq_avx, s2_avx);
+    //force Equation
+    __m256 twelve_eps_avx = _mm256_mul_ps(twelve_avx, eps_avx);
+    __m256 s6_sq_avx = _mm256_mul_ps(s6_avx,s6_avx);
+    __m256 s6_sq_sub_s6_avx =_mm256_sub_ps(s6_sq_avx, s6_avx);
+    __m256 force_fractor_avx = _mm256_div_ps(s6_sq_sub_s6_avx, r_avx);
+    __m256 force_contribution_avx = _mm256_mul_ps(twelve_eps_avx,force_fractor_avx);
 
-        temp =_mm256_add_ps(zeros,force_avx);
-        force_avx = _mm256_add_ps(force_contribution_avx, temp);
-    }
-
-    _mm256_store_ps(force_result, force_avx);
-    for (int i = 0; i < 8; i++) {
-      force += force_result[i];
-    }
-
-    return force;
+    //force_avx = _mm256_add_ps(force_avx, force_contribution_avx);
+    temp =_mm256_add_ps(zeros,force_avx); //avoid dependencies with temp & zeros
+    force_avx = _mm256_add_ps(force_contribution_avx, temp);
+  }
+  assert(((size_t)force_result) % 32 == 0); //assuming alignment
+  _mm256_store_ps(force_result, force_avx);
+  for (int i = 0; i < 8; i++) {
+    //reduction on final result, with -Ofast flag it has automatic vectorization
+    force += force_result[i];
+  }
+  return force;
 }
 
 int main(int argc, const char** argv)
@@ -82,13 +73,13 @@ int main(int argc, const char** argv)
   /// init random number generator
   srand48(1);
 
-  float *positions __attribute__((aligned(32)));
-  positions = malloc(N*sizeof(float));
+  /*float *positions __attribute__((aligned(32)));
+  positions = malloc(N*sizeof(float));*/
+  float *positions = aligned_alloc(32, N * sizeof(float));
 
+  assert(((size_t)positions) % 32 == 0); //assuming alignment
   for (size_t i=0; i<N; i++)
     positions[i] = drand48()+0.1;
-
-  //__attribute__((aligned(32))) float *positions[N];
   /// timings
   double start, end;
 
@@ -103,7 +94,6 @@ int main(int argc, const char** argv)
       f0[j] += compute_force(positions, x0[j]);
   }
   end = get_wtime();
-
   for(size_t j = 0; j < 3; ++j )
     printf("Force acting at x_0=%lf : %lf\n", x0[j], f0[j]/repetitions);
 
