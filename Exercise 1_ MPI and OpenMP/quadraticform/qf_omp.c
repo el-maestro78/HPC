97fb39dc606faa74055864bcec63f66faa177f64
CPP-Bot-78 <kzachoulitis@gmail.com>
2024-01-19 17:15:22 +0200

    quadratic form, tested for omp, not ready

--- Exercise 1_ MPI and OpenMP/quadraticform/qf_omp.c
+++ Exercise 1_ MPI and OpenMP/quadraticform/qf_omp.c
@@ -1,23 +1,14 @@
-// todo: parallelize the sequential code
+//As a templete, it is working
 #include <stdio.h>
 #include <stdlib.h>
 #include <omp.h>
-#include <mpi.h>
 
 int main(int argc, char** argv)
 {
 	int n = 16384;
- 	int rank, size;
-    MPI_Comm_size(MPI_COMM_WORLD, &size);
-    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
 	if (argc == 2)
 		n = atoi(argv[1]);
-
-
-    const unsigned int start = n*rank / size;
-    const unsigned int end = n*(rank+1) / size;
-    const unsigned int chunk_size = end - start;
 
 	// allocate memory
 	double *A = (double *)malloc(n*n*sizeof(double));
