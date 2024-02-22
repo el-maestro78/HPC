73f9ecfaaabf35dd4b69eaac023fcea1a59a48cf
CPP-Bot-78 <kzachoulitis@gmail.com>
2024-02-22 19:58:14 +0200

    qf works now

--- Exercise 1_ MPI and OpenMP/quadraticform/qf_hybrid.c
+++ Exercise 1_ MPI and OpenMP/quadraticform/qf_hybrid.c
@@ -1,4 +1,3 @@
-//#include "async/mpi.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <mpi.h>
@@ -7,11 +6,9 @@
 int main(int argc, char** argv)
 {
   int rank, size, provided ;
-  int n = 16384;
+  const unsigned int n = 16384;
 
   MPI_Init_thread(&argc, &argv, MPI_THREAD_FUNNELED, &provided);
-  MPI_Comm_size(MPI_COMM_WORLD, &size);
-  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
   if (provided < MPI_THREAD_FUNNELED) {
     printf("Error: MPI threading level is not enough.\n");
@@ -19,54 +16,68 @@ int main(int argc, char** argv)
     return 1;
   }
 
-    double *A = (double *)malloc(n*n*sizeof(double));
-    double *v = (double *)malloc(n*sizeof(double));
-    double *w = (double *)malloc(n*sizeof(double));
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
-    int local_n = n / size;
-    double *local_A = (double *)malloc(local_n * sizeof(double));
+    const unsigned int start = n*rank / size;
+    const unsigned int end = n*(rank+1) / size;
+    const unsigned int chunk_size = end - start;
 
-    // Scatter the global array 'A' to local arrays
-    MPI_Scatter(A, local_n, MPI_DOUBLE, local_A, local_n, MPI_DOUBLE, 0, MPI_COMM_WORLD);
-    #pragma omp parallel for simd
-    for (int i = 0; i < local_n; ++i) {
+    //double *A = (double *)malloc(n*n*sizeof(double));
+    //double *v = (double *)malloc(n*sizeof(double));
+    //double *w = (double *)malloc(n*sizeof(double));
+
+    //unsigned int local_n = n / size;
+    double *A = (double *)malloc(chunk_size * chunk_size * sizeof(double));
+    double *v = (double *)malloc(chunk_size * sizeof(double));
+    double *w = (double *)malloc(chunk_size * sizeof(double));
+
+#pragma omp parallel for simd collapse(2)
+    for (unsigned int i=0; i<chunk_size; ++i)
+        for (unsigned int j=0; j<chunk_size; ++j)
+            A[i * chunk_size + j] = (i + 2.0 * j) / (chunk_size * chunk_size);
+
+
+#pragma omp parallel for simd
+    for (unsigned int i = 0; i < chunk_size; ++i) {
         v[i] = 1.0 + 2.0 / (i + 0.5);
-        w[i] = 1.0 - i / (3.0 * n);
+        w[i] = 1.0 - i / (3.0 * chunk_size);
     }
 
     // Compute the result locally using OpenMP
     double local_result = 0.0;
-#pragma omp parallel for reduction(+:local_result) simd
-    for (int i = 0; i < local_n; ++i) {
-      for (int j = 0; j < n; ++j) {
-        local_result += v[i] * local_A[i] * w[j];
+#pragma omp parallel for reduction(+:local_result) simd collapse(2)
+    for (unsigned int i = 0; i < chunk_size; ++i) {
+      for (unsigned int j = 0; j < chunk_size; ++j) {
+        local_result += v[i] * A[i*chunk_size + j] * w[j];
       }
     }
 
-    // Gather the results from all processes
+    /*
     double *results = NULL;
     if (rank == 0) {
       results = (double *)malloc(size * sizeof(double));
     }
-    //MPI_Gather(&local_result, 1, MPI_DOUBLE, results, 1, MPI_DOUBLE, 0, MPI_COMM_WORLD);
-    MPI_Allreduce(&local_result, &results,1, MPI_DOUBLE, MPI_SUM, MPI_COMM_WORLD);
-    // Print the result on the root process
+     */
+    /*
     if (rank == 0) {
-      double total_result = 0.0;
-      for (int p = 0; p < size; ++p) {
-        total_result += results[p];
-      }
-      printf("Total Result = %lf\n", total_result);
-      free(results);
-    }
+        double total_result = 0.0;
+        for (int p = 0; p < size; ++p) {
+            total_result += results[p];
+        }
+        */
+    //MPI_Gather(&local_result, 1, MPI_DOUBLE, results, 1, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+    double results = 0.0;
+    MPI_Reduce(&local_result, &results, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
 
-    // Free memory
-    free(local_A);
+    if (rank == 0)
+      printf("Total Result = %lf\n", results);
+      //free(results);
     free(A);
     free(v);
     free(w);
-
     MPI_Finalize();
+
 
     return 0;
 }
